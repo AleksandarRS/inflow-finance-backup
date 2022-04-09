c27d95692fa92d966419eaae4a5f7e7526b6758b
Aleksandar Vuckovic <avuckovic85@gmail.com>
2022-04-09 12:26:03 +0200

    Add footer and start wit styling

--- wp-content/themes/inflow-theme/footer.php
+++ wp-content/themes/inflow-theme/footer.php
@@ -12,8 +12,11 @@
 				<?php 
 					$section_descriptin = get_field('section_descriptin', 'option');
 					$after_form_section = get_field('after_form_section', 'option');
+
+					$add_section_left_image = get_field('add_section_left_image', 'option');
+					$add_section_right_image = get_field('add_section_right_image', 'option');
 				?>
-				<section class="contact-details-section custom-form-elements-position">
+				<section class="contact-details-section custom-form-elements-position relative">
 					<div class="contact-details-section-wrapper section-wrapper">
 						<div class="container-wide">
 							<div class="row contact-details-row">
@@ -33,15 +36,28 @@
 										</div>
 									</div>
 								<?php endif ?>
-							</div>
+							</div> <!-- /.row contact-details-row -->
+						</div> <!-- /.container-wide -->
+						
+					</div> <!-- /.contact-details-section-wrapper section-wrapper -->
+					<?php  if ( $add_section_left_image ) : ?>
+						<div class="contact-section-decoration-image contact-section-decoration-image-left">
+							<img src="<?php echo esc_url($add_section_left_image['url']); ?>" alt="<?php echo esc_attr($add_section_left_image['alt']); ?>">
 						</div>
-					</div>
-				</section>
+					<?php endif ?>
+					<?php  if ( $add_section_right_image ) : ?>
+						<div class="contact-section-decoration-image contact-section-decoration-image-right">
+							<img src="<?php echo esc_url($add_section_right_image['url']); ?>" alt="<?php echo esc_attr($add_section_right_image['alt']); ?>">
+						</div>
+					<?php endif ?>
+				</section> <!-- /.contact-details-section custom-form-elements-position relative -->
 			</div><!-- #content -->
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
 				<div class="widget-wrapper">
 					<div class="container">
+					<!-- <i class="icon icon-check-ok"></i> -->
+					<i class="icon icon-check-solid"></i>
 						<div class="row footer-widgets-wrapper">
 							<?php get_template_part('template-parts/footer', 'widgets'); ?>
 						</div>
